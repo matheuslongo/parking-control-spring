ff0c71fea6f965ce2eb5d09ebfcbfeb72ea84cf8
matheuslongo <longo.matheusmacedo@gmail.com>
2022-07-20 17:44:54 -0300

    Get Swagger

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -11,7 +11,7 @@ public class ParkingControlApplication {
 
 	public static void main(String[] args) {SpringApplication.run(ParkingControlApplication.class,args);}
 
-		@GetMapping("/")
+		@GetMapping("/swagger-ui/index.html")
 		public String index(){
 			return "Acesse meu link do GitHub para ver as funcionalidades desta API, ela necessita de um banco de dados PostgreSql para persistir os dados." +
 					" https://github.com/matheuslongo/parking-control-spring";
